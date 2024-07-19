304c853d486af711d0f870a7b5b26dc66b043987
Kolchin13D <kolchin13d@gmail.com>
2024-07-19 12:45:18 +0300

    edit layouts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,4 +40,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    implementation("com.google.mlkit:translate:17.0.2")
+
+    
 }
